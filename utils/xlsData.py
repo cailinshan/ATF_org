6ceade1ec71573b58d7bfda36ef1815859266767
cailinshan <cailinshan@126.com>
2020-02-14 18:26:35 +0800

    add venv/lib

--- utils/xlsData.py
+++ utils/xlsData.py
@@ -9,10 +9,11 @@ def readRow(xls_name, row=0):
     read row's data
     :param row: the index of excel table
     :return: row_values
+    example: readRow(r'../x.xls',1)  or  readRow(r'../x.xls',0)[0]
     '''
     book = xlrd.open_workbook(xls_name, 'r')
     table = book.sheet_by_index(0)
     return table.row_values(row)
 
-print(readRow(r'../x.xls',1))
-print('key[{}].value[{}]'.format(readRow(r'../x.xls',0)[0],readRow(r'../x.xls',0)[1]))
+#print(readRow(r'../x.xls',1))
+
